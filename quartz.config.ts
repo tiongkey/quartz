c334905c43b79dbafff0bdbf215b9d5edc23e5a3
Gio <gqtiongson@gmail.com>
2023-08-31 15:28:55 +0800

    Quartz sync: Aug 31, 2023, 3:28 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "giotiongson 🌐com",
+    pageTitle: "giotiongson🌐com",
     enableSPA: true,
     enablePopovers: true,
     analytics: null,
